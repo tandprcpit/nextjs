4f051adaad18779c3840c8aadee081471db12779
raj.kadam <merajkadam@gmail.com>
2025-07-27 01:59:59 +0530

    Student sorting on passout year, Untitled Ui library addOn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import AuthProvider from '../context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
 import Header from '@/components/dashboard/Header';
 import Footer from '@/components/dashboard/Footer';
 import FloatingChatbot from '@/components/dashboard/FloatingChatbot';
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
